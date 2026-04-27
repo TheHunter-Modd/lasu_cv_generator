d6f306b98c0c5ae9ba57b9b09b536930b8d99a98
Victor <thehunter.modd@gmail.com>
2026-04-27 17:49:25 +0100

    login page

---
+++ login.php
@@ -0,0 +1,20 @@
+<?php
+require_once 'includes/config_session.inc.php';
+require_once 'includes/signin_view.inc.php';
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <div>
+        <form action="">
+
+    </form>
+    </div>
+</body>
+</html>
